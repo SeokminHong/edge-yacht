4b7bd46265b51122bc0594c5f6500d7c4a35eacb
Seokmin Hong <ghdtjrals240@naver.com>
2021-10-17 22:59:01 +0900

    Fix missing colon

--- packages/www/src/utils/api.ts
+++ packages/www/src/utils/api.ts
@@ -1,5 +1,5 @@
 const isDev = process.env.NODE_ENV === 'development';
-const httpProtocol = isDev ? 'http:' : 'https';
+const httpProtocol = isDev ? 'http:' : 'https:';
 const wsProtocol = isDev ? 'ws:' : 'wss:';
 const apiHost = isDev ? 'localhost:8787' : 'yacht-api.seokmin.workers.dev';
 
