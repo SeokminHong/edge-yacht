e1ffc55adea5a7b8b0f773f929f83a013a0923d3
Seokmin Hong <ghdtjrals240@naver.com>
2021-10-20 13:32:49 +0900

    Add simple router

---
+++ packages/router/rollup.config.js
@@ -0,0 +1,18 @@
+import { terser } from 'rollup-plugin-terser';
+// plugin-node-resolve and plugin-commonjs are required for a rollup bundled project
+// to resolve dependencies from node_modules. See the documentation for these plugins
+// for more details.
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    exports: 'named',
+    format: 'es',
+    file: 'dist/worker.mjs',
+    sourcemap: true,
+  },
+  plugins: [typescript(), commonjs(), nodeResolve({ browser: true }), terser()],
+};
---
+++ packages/router/src/index.ts
@@ -0,0 +1,27 @@
+// eslint-disable-next-line
+type Env = {};
+
+export default {
+  async fetch(request: Request, env: Env): Promise<Response> {
+    return handleRequest(request);
+  },
+};
+
+async function handleRequest(request: Request) {
+  // This proxies your Pages application under the condition that your Worker script is deployed on the same custom domain as your Pages project
+  const response = await fetch(request);
+
+  // Clone the response so that it is no longer immutable
+  const newResponse = new Response(response.body, response);
+
+  // Add a custom header with a value
+  newResponse.headers.append(
+    'x-workers-hello',
+    'Hello from Cloudflare Workers'
+  );
+
+  // Adjust the value for an existing header
+  newResponse.headers.set('x-header-to-change', 'NewValue');
+
+  return newResponse;
+}
