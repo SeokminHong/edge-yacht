2e3d607949df8682060b61522eb263bc37fb9f7a
Seokmin Hong <ghdtjrals240@naver.com>
2021-10-20 13:57:52 +0900

    Implement simple router for testing

---
+++ packages/router/src/env.ts
@@ -0,0 +1,7 @@
+export type Env = {
+  AUTH0_DOMAIN: string;
+  AUTH0_CLIENT_ID: string;
+  AUTH0_CLIENT_SECRET: string;
+  AUTH_STORE: KVNamespace;
+  SALT: string;
+};
--- packages/router/src/index.ts
+++ packages/router/src/index.ts
@@ -1,32 +1,17 @@
-type Env = {
-  AUTH0_DOMAIN: string;
-  AUTH0_CLIENT_ID: string;
-  AUTH0_CLIENT_SECRET: string;
-  AUTH_STORE: KVNamespace;
-  SALT: string;
-};
+import { Router } from 'itty-router';
+
+import { Env } from './env';
+
+const router = Router();
+
+router.get('/login', async (request: Request, env: Env) => {
+  return new Response('new login!');
+});
+
+router.get('*', async (request: Request) => await fetch(request));
 
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
-    return handleRequest(request);
+    return router.handle(request, env);
   },
 };
-
-async function handleRequest(request: Request) {
-  // This proxies your Pages application under the condition that your Worker script is deployed on the same custom domain as your Pages project
-  const response = await fetch(request);
-
-  // Clone the response so that it is no longer immutable
-  const newResponse = new Response(response.body, response);
-
-  // Add a custom header with a value
-  newResponse.headers.append(
-    'x-workers-hello',
-    'Hello from Cloudflare Workers'
-  );
-
-  // Adjust the value for an existing header
-  newResponse.headers.set('x-header-to-change', 'NewValue');
-
-  return newResponse;
-}
