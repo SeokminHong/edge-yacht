c24fa42b394541a9d1db3978176fd47379bf52c8
Seokmin Hong <ghdtjrals240@naver.com>
2021-10-20 13:40:03 +0900

    Retrigger action

--- packages/router/src/index.ts
+++ packages/router/src/index.ts
@@ -1,5 +1,10 @@
-// eslint-disable-next-line
-type Env = {};
+type Env = {
+  AUTH0_DOMAIN: string;
+  AUTH0_CLIENT_ID: string;
+  AUTH0_CLIENT_SECRET: string;
+  AUTH_STORE: KVNamespace;
+  SALT: string;
+};
 
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
