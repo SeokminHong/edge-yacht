5b89a824e658a140c5ef52fb5bdb1075d7395582
Seokmin Hong <ghdtjrals240@naver.com>
2021-10-22 03:03:34 +0900

    Fix build error

--- packages/www/src/audios/index.ts
+++ packages/www/src/audios/index.ts
@@ -1,38 +1,40 @@
+import { isSSR } from '~utils/window';
+
 import diceRoll01 from './dice_roll_01.mp3';
 import diceRoll02 from './dice_roll_01.mp3';
 import diceShake01 from './dice_shake_01.mp3';
 import diceShake02 from './dice_shake_02.mp3';
 
-const AUDIO_COUNT = 5;
-
-// Clone an audio pool of audio for play simultaneously
+// Create clone copies of audio for playing simultaneously
 export class AudioPlayer {
   audios: HTMLAudioElement[];
   counter: number;
+  instances: number;
 
-  constructor(url: string) {
-    const audio = new Audio(url);
-    this.audios = [
-      audio,
-      audio.cloneNode() as HTMLAudioElement,
-      audio.cloneNode() as HTMLAudioElement,
-      audio.cloneNode() as HTMLAudioElement,
-      audio.cloneNode() as HTMLAudioElement,
-    ];
+  /** @param instances should be larger than 1 */
+  constructor(url: string, instances: number) {
+    this.instances = instances;
     this.counter = 0;
+    if (isSSR) {
+      this.audios = [];
+      return;
+    }
+    const audio = new Audio(url);
+    this.audios = Array(instances - 1).fill((() => audio.cloneNode())());
+    this.audios.push(audio);
   }
 
   play() {
     this.audios[this.counter].play();
-    this.counter = (this.counter + 1) % AUDIO_COUNT;
+    this.counter = (this.counter + 1) % this.instances;
   }
 }
 
 // Roll is an once-off audio, so it doesn't need to be pooled
 export const diceRoll = [diceRoll01, diceRoll02].map(
-  (audio) => new Audio(audio)
+  (audio) => new AudioPlayer(audio, 1)
 );
 
 export const diceShake = [diceShake01, diceShake02].map(
-  (audio) => new AudioPlayer(audio)
+  (audio) => new AudioPlayer(audio, 5)
 );
